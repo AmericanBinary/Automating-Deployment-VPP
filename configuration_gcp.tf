59072e418e463db76f716357e91b4dc97556c7b0
Andrew McElroy <sophrinix@gmail.com>
2023-12-21 13:43:07 -0500

    Update configuration_gcp.tf

--- configuration_gcp.tf
+++ configuration_gcp.tf
@@ -1,1 +1,30 @@
-# High Priority TBD. pull requests welcome
+
+# Google Cloud Provider Configuration
+provider "google" {
+  # Add your project ID here
+  project = "YOUR_GCP_PROJECT_ID"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+# Define your variables here based on the VPP scenario
+
+# Creating a VPC
+resource "google_compute_network" "vpp_vpc" {
+  # Replace with your desired VPC name
+  name                    = "vpp-network"
+  auto_create_subnetworks = false # Set to true to create subnets automatically
+}
+
+#  Creating a Subnet
+resource "google_compute_subnetwork" "vpp_subnet" {
+  # Replace with your desired Subnet name
+  name          = "vpp-subnet"
+  network       = google_compute_network.vpp_vpc.name
+  # Replace with the Subnet CIDR block from the VPP scenario
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  # Define additional subnet properties as needed
+}
+
+# Add additional resources like Compute Instances, Load Balancers, Firewalls, etc., as per your VPP scenario requirements
