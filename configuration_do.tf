fecd4491f565af5c29e559e6b6cfce30ea62c57e
Andrew McElroy <amcelroy@ambit.inc>
2023-12-21 15:27:56 -0500

    we need to test but this likely works

--- configuration_do.tf
+++ configuration_do.tf
@@ -5,23 +5,65 @@ provider "digitalocean" {
   token = "YOUR_DO_TOKEN"
 }
 
-# Define your variables here based on the VPP scenario
-
-# Creating a VPC
-resource "digitalocean_vpc" "vpp_vpc" {
-  # Replace with your desired VPC name and region
-  name     = "vpp-vpc"
-  region   = "nyc3"
-  # Replace with the VPC CIDR block from the VPP scenario
-  ip_range = "10.0.0.0/16"
+# Digital Ocean VPC (similar to aws_vpc)
+resource "digitalocean_vpc" "do_vpc" {
+  name        = "do-vpc"
+  region      = "nyc3"
+  ip_range    = "10.11.0.0/16"
 }
 
-#  Creating a Subnet
-resource "digitalocean_subnet" "vpp_subnet" {
-  # Replace with your desired Subnet name and region
-  vpc_uuid  = digitalocean_vpc.vpp_vpc.id
-  region    = "nyc3"
-  # Replace with the Subnet CIDR block from the VPP scenario
-  ip_range  = "10.0.1.0/24"
-  # Define additional subnet properties as needed
+# Digital Ocean Subnets (similar to aws_subnet)
+resource "digitalocean_vpc_subnet" "do_subnet_vpp" {
+  vpc_id      = digitalocean_vpc.do_vpc.id
+  ip_range    = "10.11.1.0/24"
 }
+
+resource "digitalocean_vpc_subnet" "do_subnet_management" {
+  vpc_id      = digitalocean_vpc.do_vpc.id
+  ip_range    = "10.11.2.0/24"
+}
+
+# Digital Ocean does not support IPv6 in the same manner as AWS
+# The ipv6 subnet will be omitted in the Digital Ocean configuration
+
+# Digital Ocean Firewall (similar to aws_security_group)
+resource "digitalocean_firewall" "do_firewall_allow_all" {
+  name = "allow-all"
+
+  inbound_rule {
+    protocol    = "tcp"
+    port_range  = "1-65535"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol        = "tcp"
+    port_range      = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Attach the firewall to the droplets
+  droplet_ids = [digitalocean_droplet.do_instance_vpp.id]
+}
+
+# Digital Ocean Droplet (similar to aws_instance)
+resource "digitalocean_droplet" "do_instance_vpp" {
+  image  = "ubuntu-20-04-x64"
+  name   = "do-instance-vpp"
+  region = "nyc3"
+  size   = "s-1vcpu-2gb"
+  private_networking = true
+  ssh_keys = [digitalocean_ssh_key.do_ssh_key.id]
+
+  # Assign it to the management subnet
+  vpc_uuid = digitalocean_vpc.do_vpc.id
+}
+
+# Digital Ocean SSH Key (to replace aws_key_pair)
+resource "digitalocean_ssh_key" "do_ssh_key" {
+  name       = "do_ssh_key"
+  public_key = "ssh-rsa YOUR_SSH_PUBLIC_KEY"
+}
+
+# Digital Ocean does not have a direct equivalent of AWS Network Interface or EIP.
+# These components are omitted in the Digital Ocean configuration.
--- configuration_gcp.tf
+++ configuration_gcp.tf
@@ -1,30 +1,77 @@
 
-# Google Cloud Provider Configuration
+# GCP Provider Configuration
 provider "google" {
-  # Add your project ID here
-  project = "YOUR_GCP_PROJECT_ID"
+  project = "your-gcp-project-id"
   region  = "us-central1"
   zone    = "us-central1-a"
 }
 
-# Define your variables here based on the VPP scenario
-
-# Creating a VPC
-resource "google_compute_network" "vpp_vpc" {
-  # Replace with your desired VPC name
-  name                    = "vpp-network"
-  auto_create_subnetworks = false # Set to true to create subnets automatically
+# VPC Network (similar to azurerm_virtual_network)
+resource "google_compute_network" "vpc_network" {
+  name                    = "gcp-vpc-network"
+  auto_create_subnetworks = false # We'll define subnetworks manually
 }
 
-#  Creating a Subnet
-resource "google_compute_subnetwork" "vpp_subnet" {
-  # Replace with your desired Subnet name
-  name          = "vpp-subnet"
-  network       = google_compute_network.vpp_vpc.name
-  # Replace with the Subnet CIDR block from the VPP scenario
+# Subnets (similar to azurerm_subnet)
+resource "google_compute_subnetwork" "subnet_mgmt" {
+  name          = "subnet-mgmt"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.name
+}
+
+resource "google_compute_subnetwork" "subnet_vpp" {
+  name          = "subnet-vpp"
   ip_cidr_range = "10.0.2.0/24"
   region        = "us-central1"
-  # Define additional subnet properties as needed
+  network       = google_compute_network.vpc_network.name
 }
 
-# Add additional resources like Compute Instances, Load Balancers, Firewalls, etc., as per your VPP scenario requirements
+# Public IP (similar to azurerm_public_ip)
+resource "google_compute_address" "public_ip" {
+  name   = "gcp-public-ip"
+  region = "us-central1"
+}
+
+# Firewall Rules (similar to azurerm_network_security_group)
+resource "google_compute_firewall" "firewall" {
+  name    = "gcp-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Compute Instance (similar to azurerm_virtual_machine)
+resource "google_compute_instance" "vm_instance" {
+  name         = "gcp-vm-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20210817"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet_mgmt.name
+
+    access_config {
+      // Ephemeral public IP
+      nat_ip = google_compute_address.public_ip.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  tags = ["http-server", "https-server"]
+}
+
+# Additional resources like NICs, route tables, etc., can be configured as needed
